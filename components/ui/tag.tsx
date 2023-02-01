c05bea7f043df3965ba31ab1236b7ba8090273ed
Sam Huckaby <samhuckaby@gmail.com>
2023-01-31 22:26:19 -0500

    Should have made that property optional

--- components/ui/tag.tsx
+++ components/ui/tag.tsx
@@ -3,7 +3,7 @@ import type {ReactNode} from 'react';
 const Tag = ({
 	children,
 	className
-	}: {children?: ReactNode, className: string}) => {
+	}: {children?: ReactNode, className?: string}) => {
 	return (
 		<span className={`${className} text-xs inline-block py-1 px-2.5 mx-1 leading-none text-center whitespace-nowrap align-baseline font-bold bg-gray-100 text-gray-700 rounded-md`}>
 			{children}
