5d1110b7e761aa49c9b693ded4c0c36ab4b30e41
Sam Huckaby <samhuckaby@gmail.com>
2023-02-07 00:08:42 -0500

    Add a google font, so things are less private

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -1,20 +1,27 @@
 import Link from 'next/link';
 import MainNavigation from './main-navigation';
 import type {SiteSettings} from '~/models/site-settings';
+import { Encode_Sans_SC } from "@next/font/google";
+
+const encodeSansSC = Encode_Sans_SC({
+  weight: "400",
+  subsets: ['latin']
+});
 
 const Header = ({siteSettings}: {siteSettings: SiteSettings}) => {
 	return (
-		<header className="horizontal-wipeout
+		<header className={`horizontal-wipeout
 			mx-auto max-w-5xl
 			pl-12 pr-8 pt-8
 			flex flex-row items-start
-			border-b border-b-slate-200">
+			border-b border-b-slate-200
+      ${encodeSansSC.className}`}>
 			<Link href='/' className="-ml-12 pl-12">
 				<div className="
 					flex flex-row items-center
 					border-4 border-black rounded-t-full
 					h-[64px] w-[64px]">
-					<span className="text-2xl bg-white -ml-[43px]">OSS.<span className="text-black font-bold">rip</span></span>
+					<span className="text-2xl bg-white -ml-[37px] -mt-[12px]">OSS.</span><span className="text-black font-bold text-2xl">rip</span>
 				</div>
 			</Link>
 			<MainNavigation navigation={siteSettings.navigation} />
